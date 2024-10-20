4b5b49f3f19444722acb7f2d1e417bfbf6ee076a
sabbir-sabbir <iosabbir7890@gmail.com>
2024-10-20 11:43:37 +0600

    new commit

--- page/src/components/Incidents.jsx
+++ page/src/components/Incidents.jsx
@@ -5,6 +5,7 @@ import Dashboard from './Dashboard'
 export default function Incidents() {
   return (
     <>
+    {/* Dashboard Again */}
    <Dashboard/>
    <section>
     <div className="w-full h-32 ">
