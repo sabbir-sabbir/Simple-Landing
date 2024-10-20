2f2047b8a585620cb88ac073cfa08f292570727d
sabbir-sabbir <iosabbir7890@gmail.com>
2024-10-20 11:27:49 +0600

    new commit

--- page/src/App.jsx
+++ page/src/App.jsx
@@ -1,13 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AOS from 'aos';
+import About from "./components/Incidents";
+import Location from "./components/Location";
+import Error from "./components/Error";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import SignUp from "./components/SignUp";
 
-import './App.css'
+
+
 function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="" element={<Navbar />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/incidents" element={<About />} />
+          <Route path="/location" element={<Location />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/cypher" element={<SignUp />} />
+          <Route path="*" element={<Error />}></Route>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+   
+  );
 }
 
-export default App
+export default App;
---
+++ page/src/compinents/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+export default function SignUp() {
+  return (
+   
+    <>
+    <h1 class=" absolute font-extrabold text-[150px] mt-[250px] bg-gradient-to-r from-[#ff4f00] via-[#ffea00] to-[#ffdd00] text-transparent bg-clip-text" data-aos="flip-up">
+    Not Available
+</h1>
+    <div className="  w-full h-60 opacity-30 ">
+      <img src="no.jpg" alt="image"  className="  object-cover"/>
+    </div>
+    </>
+  )
+}
